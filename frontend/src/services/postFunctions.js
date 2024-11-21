813f5ae091fce574dc864658e2d341c16ac01267
Dipesh Arwat <dipesharwat80@gmail.com>
2024-11-21 18:37:57 +0530

    chages

--- frontend/src/services/postFunctions.js
+++ frontend/src/services/postFunctions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "/";
+const BASE_URL = "https://instagram-clone-nypr.onrender.com/";
 
 export async function commentPost(postId, comment) {
   const token = localStorage.getItem("jwt");
